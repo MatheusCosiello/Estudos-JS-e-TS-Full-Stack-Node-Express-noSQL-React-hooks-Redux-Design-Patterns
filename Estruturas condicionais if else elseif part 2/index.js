42a3b71774a58f543fcfc49e774303060fdbffaa
MatheusCosiello <matheus.cosiello@gmail.com>
2022-01-10 14:59:05 -0300

    atualização aula 10/01 html + css + js

---
+++ Estruturas condicionais if else elseif part 2/index.js
@@ -0,0 +1,22 @@
+const numero = 9;
+
+if (numero >= 10){
+    console.log('numero maior que 10');
+}else {
+    console.log('numero menor que 10');
+}
+//-------------------------------------------------------------------------
+
+const numero = 50 ;
+
+if (numero>= 0 && numero <= 5){
+    console.log('numero esta entre 0 e 5')
+} else if (numero >= 6 && numero <= 8){
+    console.log('numero esta entre 6 e 8')
+} else if (numero >= 9 && numero <= 11){
+    console.log('numero esta entre 9 e 11')
+
+} else {
+    console.log('numero não esta dentro de 0 e 11')
+}
+//---------------------------------------------------------------------------------------
