6402780c8fa8e3726ad77de68d6ffbb5749d5a85
MatheusCosiello <matheus.cosiello@gmail.com>
2022-01-17 17:26:45 -0300

    att 17/01

--- calculando IMC/assets/js/main.js
+++ calculando IMC/assets/js/main.js
@@ -1,28 +1,66 @@
-// peso / altura x altura
+// Capturar evento de submit do formulário
+const form = document.querySelector('#formulario');
 
-function meuEscopo() {
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const inputPeso = e.target.querySelector('#peso');
+  const inputAltura = e.target.querySelector('#altura');
 
-    const formCLiente = document.getElementById('form');
-    const imcCLiente = document.getElementById('resultado');
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
 
-    function recebeEventoForm(evento) {
-        evento.preventDefault();
-        const pesoCliente = formCLiente.getElementById('peso');
-        const alturaCliente = formCLiente.getElementById('altura');
-        
-        function calculaIMC() {
+  if (!peso) {
+    setResultado('Peso inválido', false);
+    return;
+  }
 
-            pesoCliente / (alturaCliente * alturaCliente);
-            
-        }
-        calculaIMC();
-        console.log(pesoCliente);
-        console.log(alturaCliente);
-        console.log(calculaIMC);
+  if (!altura) {
+    setResultado('Altura inválida', false);
+    return;
+  }
 
-    }
+  const imc = getImc(peso, altura);
+  const nivelImc = getNivelImc(imc);
 
+  const msg = `Seu IMC é ${imc} (${nivelImc}).`;
 
-    formCLiente.addEventListener('submit', recebeEventoForm)
+  setResultado(msg, true);
+});
 
-} meuEscopo();
+function getNivelImc (imc) {
+  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
+    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+  if (imc >= 39.9) return nivel[5];
+  if (imc >= 34.9) return nivel[4];
+  if (imc >= 29.9) return nivel[3];
+  if (imc >= 24.9) return nivel[2];
+  if (imc >= 18.5) return nivel[1];
+  if (imc < 18.5) return nivel[0];
+}
+
+function getImc (peso, altura) {
+  const imc = peso / altura ** 2;
+  return imc.toFixed(2);
+}
+
+function criaP () {
+  const p = document.createElement('p');
+  return p;
+}
+
+function setResultado (msg, isValid) {
+  const resultado = document.querySelector('#resultado');
+  resultado.innerHTML = '';
+
+  const p = criaP();
+
+  if (isValid) {
+    p.classList.add('paragrafo-resultado');
+  } else {
+    p.classList.add('bad');
+  }
+
+  p.innerHTML = msg;
+  resultado.appendChild(p);
+}
