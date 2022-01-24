8fe06e3ac02808f68cc8e1833d995562566b2ec2
MatheusCosiello <matheus.cosiello@gmail.com>
2022-01-24 17:03:57 -0300

    att dia 24/01
    adicionando uma seção referente diferenças entre var let e const part1

---
+++ Main Diferenças entre var-let-const/main.js
@@ -0,0 +1,20 @@
+let nome = 'Matheus';
+var nome2 = 'Matheus';
+// nesse momento var e let estão faznedo a mesma coisa criando uma variavel no escopo global
+
+// let nao pode ser edeclarado 
+let nome ='Bruno'; //ERRADO
+
+//VAR PODE SER EDECLARADO
+var nome2 = 'bruno'; // CORRETO
+//--------------------------------------------------------------------------------
+const verdade = true ;
+
+if (verdade) {
+    let nome = "carlos";
+console.log(nome); 
+}
+
+//let tem scopo de bloco {... bloco}
+// var so tem escopo de função
+//-----------------------------------------------------------------------------------
