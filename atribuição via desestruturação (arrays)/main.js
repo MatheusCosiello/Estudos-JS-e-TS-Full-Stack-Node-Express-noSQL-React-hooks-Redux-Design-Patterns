ccb850be789541755c0197c408e7de77135dbf3f
MatheusCosiello <matheus.cosiello@gmail.com>
2022-01-26 14:23:23 -0300

    att dia 26/01 atribuição via arrays
    adicionando conteudo sobre a aula part

--- atribuição via desestruturação (arrays)/main.js
+++ atribuição via desestruturação (arrays)/main.js
@@ -11,4 +11,19 @@ const numero = [1,2,3];
 // maneira direta de ser feito tambem
 [A,B,C]=[1,2,3]
 //foi ATRIBUIDO a array atraves do seu indici um valor de variavel
-console.log(A,B,C);
+console.log(A,B,C);// exibe 1 2 3
+//-------------------------------------------------------------------
+//                  part 2
+//      indici  0 1 2 3 4 5 6 7 8
+const numero = [1,2,3,4,5,6,7,8,9];
+
+console.log(numero[0]); //eixibi numero 1
+
+// utilizando este metodo podemos retirar coisas do array e ja jogar em uma variavel
+
+//      indici  0 1 2 3 4 5 6 7 8
+const numero = [1,2,3,4,5,6,7,8,9];
+const [primeiroNumero, segundoNumero] = numeros;
+console.log(primeiroNumero,segundoNumero); // exibe 1 , 2
+
+// aqui foi atribuido o valor da variavel numeros as variaveis primeiroNumero, segundoNumero atraves do indici
