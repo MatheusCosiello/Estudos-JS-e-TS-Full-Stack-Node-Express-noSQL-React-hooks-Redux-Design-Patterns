2d8aabd80c79dcace95e4d6efea45fabf9d6469a
MatheusCosiello <matheus.cosiello@gmail.com>
2022-01-28 14:58:31 -0300

    att 28/01
    atribuição via desestruturação de objetos
    part 1 estrutura basica de objeto pessoa contendo um objeto endereço

---
+++ atribuição via desestruituração de objetos/main.js
@@ -0,0 +1,12 @@
+
+// objeto pessoa com objeto interno enderço
+const pessoa = {
+    nome :'Matheus',
+    sobreNome :'Cosiello',
+    endereço : {
+        rua : 'rua 1',
+        casa : 123
+    }
+};
+const nomeDaPessoa = pessoa.nome;
+console.log(nomeDaPessoa);
