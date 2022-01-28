9a0a82cb06252719f2158a41af55329889f17974
MatheusCosiello <matheus.cosiello@gmail.com>
2022-01-28 15:18:08 -0300

    att 28/01
    atribuição via desestruturação de objetos
    part 2 fazendo uma estruturação via desestruturação de objetos com 2 exemplos praticos

--- atribuição via desestruituração de objetos/main.js
+++ atribuição via desestruituração de objetos/main.js
@@ -3,10 +3,21 @@
 const pessoa = {
     nome :'Matheus',
     sobreNome :'Cosiello',
+    idade: 29,
     endereço : {
         rua : 'rua 1',
         casa : 123
     }
 };
 const nomeDaPessoa = pessoa.nome;
-console.log(nomeDaPessoa);
+console.log(nomeDaPessoa); // exibi nome matheus
+
+//---------part 2-----------------
+// atribuição via desestruturação
+
+const {nome} = pessoa;
+console.log(pessoa); //exibi nome da pessoa Matheus
+// tambem podemos adicionar mais variaveis
+
+const {nome, sobreNome, idade} = pessoa;
+console.log(pessoa); // exibi matheus, cosiello, 29
