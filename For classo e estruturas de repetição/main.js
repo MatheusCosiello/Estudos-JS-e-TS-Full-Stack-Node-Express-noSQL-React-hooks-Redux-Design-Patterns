ef9a31a6cb3329bc9bcda5b56e83f3bbd3910975
MatheusCosiello <matheus.cosiello@gmail.com>
2022-02-01 15:32:44 -0300

    att 01/02
    Esqueleto de FOR e estrutura de repetição base repetição

---
+++ For classo e estruturas de repetição/main.js
@@ -0,0 +1,11 @@
+// esqueleto para estrutura de repetição FOR
+
+// para que a estrutura de repetição possa funcionar precisamos criar dentro dele
+// dentro do parenteses
+// 1 criar uma  variavel de controle
+// 2 crair uma condição que faz ele parar
+// 3 incrementar ou decrementar o laço
+//  1 i = index     |2condição|     3incremento
+for (let i = 0;      i <= 5;         i++) {
+console.log(i);
+};
