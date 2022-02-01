0f633d012a2586e0995f613c56e982e9044bc6a0
MatheusCosiello <matheus.cosiello@gmail.com>
2022-02-01 16:01:02 -0300

    att 01/02 Esqueleto de FOR e estrutura de repetição base repetição
    Part 2 controlando a contagem a partir do FOR+ template strings

--- For classo e estruturas de repetição/main.js
+++ For classo e estruturas de repetição/main.js
@@ -8,4 +8,15 @@
 //  1 i = index     |2condição|     3incremento
 for (let i = 0;      i <= 5;         i++) {
 console.log(i);
-};
+};          
+/*                      part 2
+utilizamos a parte do for para controlar o nosso laço
+
+podemos determinar o inicio da contagem em qualquer numero na variavel
+podemos determinar o final em nossa condicional
+podemos determinar como vai ser feita a contagem em nosso incremento/decremento
+                abaixo exemplo
+*/
+for (let i = 0;      i <= 500;         i+=10) {
+    console.log(`Linha${i}`); // exibi contagem de linhas apartir de 0 a 500 contando de 10 em 10
+    };
