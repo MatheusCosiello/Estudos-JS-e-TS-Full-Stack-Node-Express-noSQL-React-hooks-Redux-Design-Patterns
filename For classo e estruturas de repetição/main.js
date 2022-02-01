8b079bb4ff1fcd43241adef6e53a123c21874b31
MatheusCosiello <matheus.cosiello@gmail.com>
2022-02-01 17:58:59 -0300

    att 01/02 Esqueleto de FOR e estrutura de repetição base repetição
    adicionando  um novo exemplo com estrutura ternaria +
    part 4 onde vemos como podemos nos movimentar atraves de um index de array utilizando for

--- For classo e estruturas de repetição/main.js
+++ For classo e estruturas de repetição/main.js
@@ -6,9 +6,9 @@
 // 2 crair uma condição que faz ele parar
 // 3 incrementar ou decrementar o laço
 //  1 i = index     |2condição|     3incremento
-for (let i = 0;      i <= 5;         i++) {
-console.log(i);
-};          
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+};
 /*                      part 2
 utilizamos a parte do for para controlar o nosso laço
 
@@ -17,33 +17,40 @@ podemos determinar o final em nossa condicional
 podemos determinar como vai ser feita a contagem em nosso incremento/decremento
                 abaixo exemplo
 */
-for (let i = 0;      i <= 500;         i+=10) {
-   console.log(`Linha${i}`); // exibi contagem de linhas apartir de 0 a 500 contando de 10 em
-    };
+for (let i = 0; i <= 500; i += 10) {
+    console.log(`Linha${i}`); // exibi contagem de linhas apartir de 0 a 500 contando de 10 em
+};
 
 //                        exemplo de decrementação
 
-for (let i = 500;      i >= 400;         i-=10) {
+for (let i = 500; i >= 400; i -= 10) {
     console.log(`Linha${i}`); // exibi contagem de linhas apartir de 500 are 400 contando de 10 em
-   };
+};
 
 //                                  part 3
 // um exemplo de uso para for e laço de repetição, verificar se o umero é par ou não
 
-for (let i = 0;i <= 10; i++) {
-    const par = i % 2 ===0;
+for (let i = 0; i <= 10; i++) {
+    const par = i % 2 === 0 ? 'par' : 'impar'; // adicionando estrutura ternaria
     console.log(i, par);
-    };
+};
 /*Exibit 
- 0 true
-1 false
-2 true
-3 false
-4 true
-5 false
-6 true
-7 false
-8 true
-9 false
-10 true 
+ 0 true 0 par
+1 false 1 impar
+2 true  2 par
+3 false 3 impar
+4 true  4 par
+5 false 5 impar
+6 true  6 par
+7 false 7 impar
+8 true  8 par
+9 false 9 impar
+10 true 10 par 
 */
+//                                  part 4
+//   podemos percorrer um array utilizando FOR
+const frutas = ['pera', 'uva', 'banana'];
+for (let i = 0; i = array.length; i++) {
+    console.log(i);
+};
+// exibi 0 1 2 que são os index do array
