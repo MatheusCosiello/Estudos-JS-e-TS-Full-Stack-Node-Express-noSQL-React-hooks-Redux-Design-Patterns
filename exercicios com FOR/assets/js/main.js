3b4dde9dce0a803cd930bb7dbc2a3c78d98fd1ae
MatheusCosiello <matheus.cosiello@gmail.com>
2022-02-07 13:55:01 -0300

    att 07/02 volta dos estudos pos recuperação com covid
    exercicios com FOR e mescla de elementos criados

---
+++ exercicios com FOR/assets/js/main.js
@@ -0,0 +1,16 @@
+// exericios com FOR
+
+const elementos = [
+    {tag : 'p', texto: 'Frase 1'},          //0
+    {tag : 'div', texto: 'Frase 2'},        //1
+    {tag : 'section', texto: 'Frase 3'},    //2
+    {tag : 'footer', texto: 'Frase 4'},     //3
+];
+
+const container = document.querySelectorAll(".container");  // seleciona a classe conteiner
+const div = document.createElement('div');
+
+// iterar os elementos do array elementos iterar = percorrer
+
+
+
