72b6e832fdb2fce92c589eb604b1f6ad9949183f
MatheusCosiello <matheus.cosiello@gmail.com>
2022-02-07 15:54:31 -0300

    att 07/02
    exercicio finalizado FOR

--- exercicios com FOR/assets/js/main.js
+++ exercicios com FOR/assets/js/main.js
@@ -7,12 +7,18 @@ const elementos = [
     {tag : 'footer', texto: 'Frase 4'},     //3
 ];
 
-const container = document.querySelectorAll(".container");  // seleciona a classe conteiner
+const container = document.querySelector(".container");  // seleciona a classe conteiner
 const div = document.createElement('div');
 
 // iterar os elementos do array elementos iterar = percorrer
 
-// 123123
+for (let i = 0; i < elementos.length; i++){
+    let {tag,texto} = elementos[i];
+    let tagCreate = document.createElement(tag);
+    tagCreate.innerHTML = texto;
+    div.appendChild(tagCreate);
+}
+container.appendChild(div);
 
 
 
