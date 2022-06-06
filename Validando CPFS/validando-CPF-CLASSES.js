38831bf11c8ef5a968b993ffae4620ab8a38a922
MatheusCosiello <matheus.cosiello@gmail.com>
2022-06-06 03:13:00 -0300

    adicionando novos metodos e adicionando novas verificações aos laços

--- Validando CPFS/validando-CPF-CLASSES.js
+++ Validando CPFS/validando-CPF-CLASSES.js
@@ -1,16 +1,35 @@
 // 705.484.450-52 || 070.987.720-03
 
 // valida o cpf
-class ValidaCPF{
-    constructor(cpfEnviado){
-        Object.defineProperty(this,'cpfLimpo',{
+class ValidaCPF {
+    constructor(cpfEnviado) {
+        Object.defineProperty(this, 'cpfLimpo', {
             writable: false,
             enumerable: false,
             configurable: false,
-            value: cpfEnviado.replace(/\D+/g,'')
+            value: cpfEnviado.replace(/\D+/g, '')
         });
+
     }
+
+    éSequencia(){
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo; // validando sequencia de numeros
+    }
+
+    geraNovoCpf(){
+         
+    }
+
+            // metodo de validação  do cpf
+    valida() {
+        if(!this.cpfLimpo) return false; //cpflimpo
+        if(typeof this.cpfLimpo !== 'string') return false; //cpf é string
+        if(this.cpfLimpo.length !== 11) return false; // valida 11 digitos
+        if(this.éSequencia()) return false; // veirfica sé é sequencia
+        if(this.geraNovoCpf()) return false // verifica se gerou novo cpf
+        return 'teste';
+    };
 }
 
-const ValidaCpf = new ValidaCPF('070.987.720-03');
-console.log(ValidaCpf.valida());
+const CPF1 = new ValidaCPF('070.987.720-03');
+console.log(CPF1.valida());
